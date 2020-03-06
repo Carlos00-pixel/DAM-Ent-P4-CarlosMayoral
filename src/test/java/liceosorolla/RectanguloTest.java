0302c76f81123bf5aa84b3a53df278e6845c6bdd
DAM_1 <DAM_1@PB01>
2020-03-06 19:13:20 +0100

    Comprobar cuadrado, atributos getters y setters

--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,24 @@
 package test.java.liceosorolla;
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Rectangulo;
+import static org.junit.Assert.assertEquals;
 
 public class RectanguloTest {
+	
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void Before() {
+		
+		rectangulo = new Rectangulo(5,5);
+		
+	}
+	
+	@Test
+	public void comprobarCuadrado() {
+		
+		assertEquals(true,rectangulo.esCuadrado());
+	}
 
 }
