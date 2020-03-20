9be05f47ac2e9fd400e35fb8547b3f253d6fbe25
Carlos <Carlos@192.168.1.39>
2020-03-20 21:04:45 +0100

    Animale

---
+++ src/main/java/liceosorolla/Animal.java
@@ -0,0 +1,27 @@
+package src.main.java.liceosorolla;
+
+public class Animal {
+	
+	private String especie;
+	private String raza;
+	private double peso;
+	private int edad;
+	private String colorPelo;
+	
+	public Animal(String especie, String raza, double peso, int edad, String colorPelo) {
+		this.especie = especie;
+		this.raza = raza;
+		this.peso = peso;
+		this.edad = edad;
+		this.colorPelo = colorPelo;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+}
