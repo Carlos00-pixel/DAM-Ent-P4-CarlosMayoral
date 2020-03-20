c1b0f3b89211878c7506a8a9c1142428abe56c12
Carlos <Carlos@192.168.1.39>
2020-03-20 21:05:04 +0100

    AnimalTest

---
+++ src/test/java/liceosorolla/AnimalTest.java
@@ -0,0 +1,27 @@
+package src.test.java.liceosorolla;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import src.main.java.liceosorolla.Animal;
+
+public class AnimalTest {
+	
+	private Animal animal;
+	
+	
+	@Before
+	public void Before() {
+		
+		animal = new Animal ("mamifero","gato",4.5,2,"negro");
+	}
+	
+	@Test
+	public void comprobarEspecie() {
+		
+		assertEquals("mamifero", animal.getEspecie());
+		
+	}
+	
+	
+}
